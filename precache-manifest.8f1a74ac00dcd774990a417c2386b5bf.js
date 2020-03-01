7fabb5c611f10ed0eb1f679d30b2a9d5824ea228
abdonaber <51922753+abdonaber@users.noreply.github.com>
2020-03-01 14:03:52 +0400

    Updates

---
+++ precache-manifest.8f1a74ac00dcd774990a417c2386b5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4600da741fe81abb4ba543386f7f8c5",
+    "url": "/first-react-deploy/index.html"
+  },
+  {
+    "revision": "1c938e865684c033c9ff",
+    "url": "/first-react-deploy/static/css/main.9e92a134.chunk.css"
+  },
+  {
+    "revision": "7564a44086665b6f6e36",
+    "url": "/first-react-deploy/static/js/2.0f53358f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/first-react-deploy/static/js/2.0f53358f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c938e865684c033c9ff",
+    "url": "/first-react-deploy/static/js/main.48d04d5a.chunk.js"
+  },
+  {
+    "revision": "8753555fb94c635b8315",
+    "url": "/first-react-deploy/static/js/runtime-main.d3877102.js"
+  }
+]);
